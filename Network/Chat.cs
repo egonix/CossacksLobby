01a4c0ad4c73fb9b0cdb71d70c809c43c6e952f2
egonix <github@egonix.de>
2016-10-03 15:18:24 +0200

    Add Chat Package

---
+++ Network/Chat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CossacksLobby.Network
+{
+    [Package(PackageNumber.ChatMessageRequest)]
+    class ChatMessageRequest
+    {
+        public string Message { get; set; }
+    }
+
+    [Package(PackageNumber.ChatMessageResponse)]
+    class ChatMessageResponse
+    {
+        public string Message { get; set; }
+    }
+}
